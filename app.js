1ec3ac9d681097ad06c25c2805b8dcb7c67541e1
Niels Daemen <niels.747@hotmail.com>
2022-06-23 18:11:30 +0200

    Changed file name

--- app.js
+++ app.js
@@ -1892,7 +1892,7 @@ async function mainScript(
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-  imgElement = await loadImage("resources/forestFire.png");
+  imgElement = await loadImage("resources/forestfire.png");
   const forestFireTexture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, forestFireTexture);
   gl.texImage2D(
